b8e376368202076fca6564853c8873cbb58f9e71
nyamekye123 <38310193+nyamekye123@users.noreply.github.com>
2018-10-05 12:49:13 +0200

    Add files via upload

---
+++ funzione.js
@@ -0,0 +1,25 @@
+/* funzioni per CRT */
+function calcola(){
+	/* valori del form */
+	var C = document.forms.crt.C.value;
+	var r = document.forms.crt.r.value;
+	var T = document.forms.crt.T.value;
+	var U = document.forms.crt.U.value;
+	
+	
+	if(){
+		document.getElementById("risultato").innerHTML="Errore";
+	}
+	else if(U = 1){
+		var d = 36500;
+		
+	}
+	else if(U = 2){
+		var d = 1200;
+		
+	}
+	else if(U = 3){
+		var d = 100;
+	}
+	
+	I = (9
